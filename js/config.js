0e701be4fecf22956768fcc1ec1148f76e6facd3
Abdul Ghaffar <abdulghaffar15a@gmail.com>
2026-07-09 03:51:17 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_iM1IqWWca', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4bc1kiv2hu7oredhm5bjcsushl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '39g0cuilitsnsmkplun9dnlans', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
